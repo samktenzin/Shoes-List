1af3e0ff8319eb319d9bf141605488fafce2b69d
Sam Khanal <samkhanal@iclooud.com>
2020-11-16 07:35:02 -0500

    Frontend Adjustments

--- sneakergram/src/Components/AddYeezy/AddYeezy.js
+++ sneakergram/src/Components/AddYeezy/AddYeezy.js
@@ -1,6 +1,26 @@
 import React from "react";
 
 class AddYeezy extends React.Component{
+    constructor (props) {
+        super(props);
+
+        this.onChangeAddYeezyDescription = this.onChangeAddYeezyDescription.bind(this);
+        this.onChangeAddYeezyImg = this.onChangeAddYeezyImg.bind(this);
+        this.onChangeAddYeezyPriority = this.onChangeAddYeezyPriority.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            AddYeezy_description: '',
+            AddYeezy_img: '',
+            AddYeezy_priority: '',
+            AddYeezy_released: false
+        }
+        //Setting the initial state of the component
+        //with the properties above.
+    }
+
+
+    
     render () {
         return (
             <div>
